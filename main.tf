d031b73da9a5947c17434072d1e46632bf74bd18
root <root@ip-172-31-44-46.ap-south-1.compute.internal>
2023-04-27 07:45:36 +0000

    dd

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+locals {
+  key = "mykey.pem"
+}
 provider "aws" {
  region = "ap-south-1"
 }
@@ -5,12 +8,12 @@ resource "aws_instance" "ec2-server" {
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
   key_name = "mykey"
-  vpc_security_group_ids= ["sg-00fd2fb52aa7dd027"]
+  vpc_security_group_ids= ["sg-00fd2fb52aa7dd027"]ii
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("mykey.pem")
-    host  =  "aws_instance.ec2-server.public_ip"
+    private_key = file(local.key)
+    host  =  aws_instance.ec2-server.public_ip
   }
   tags = {
     Name = "terraform"
@@ -19,6 +22,6 @@ resource "aws_instance" "ec2-server" {
         command = " echo ${aws_instance.ec2-server.private_ip} > inventory "
   }
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/project/bank-playbook.yml "
+    command = "ansible-playbook -i ${aws_instance.ec2-server.private_ip}, --private-key ${local.key} bank-deploy.yml"
   }
 }
